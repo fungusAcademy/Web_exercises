4f7277e4388b430d86e11d30995a571ab3158ba6
fungusAcademy <grivus12@yandex.ru>
2018-10-16 22:07:33 +1000

    HTML: minor design changes JS: 1. Keystroke feature     2. Code refactoring

--- Additional tasks/Task_1/script.js
+++ Additional tasks/Task_1/script.js
@@ -1,57 +1,48 @@
-function GetRandomNumber(min, max) 
-{ 
-    return min + Math.random() * (max - min);
-}
-
-const Direction = Object.freeze(
+const DIRECTION = Object.freeze(
 {
-    LEFT: "LEFT",
+    LEFT:  "LEFT",
     RIGHT: "RIGHT",
 });
 
-const Colors = Object.freeze(
+const COLORS = Object.freeze(
 {
-    RED: "#FF0000",
-    WHITE: "#FFFFFF",
-    BLACK: "#000000",
+    RED:    "#FF0000",
+    WHITE:  "#FFFFFF",
+    BLACK:  "#000000",
     YELLOW: "#FFFF00",
-    BLUE: "#0000FF",
+    BLUE:   "#0000FF",
 });
 
-const SIZE = 15;
-const STARTSPEED = 0.25;
-const NEXTLEVEL = 200;
+const SIZE       = 15;
+const STARTSPEED = 0.5;
+const NEXTLEVEL  = 200;
+const LEFTBORDER = 50; // ball start pos?
+const STARTSCORE = 0;
+const STARTLEVEL = 1;
 
 var canvas, ctx;
 var Items = [], Gun, GunColor;
 var idTimer = -1, Timer = -1, GunTimer = -1, reloadGun = 1;
-var direction = Direction.LEFT;
-var score = 0;
-var level = 1;
-var randClr = Colors.RED;
-//Place in Init()
+var best, score, level, randClr;
 
-function GetRandomColor() 
-{
-    let keys = Object.keys(Colors);
-
-    return Colors[keys[Math.floor(keys.length * Math.random())]];
-}
-
+/* TBall class */
 class TBall 
 {
-    constructor(pX, pY, dir) 
+    constructor(pX, pY, dir, color) 
     {
         this.radius = SIZE;
-        this.posX = pX;
-        this.posY = pY;
-        this.speed = STARTSPEED + Math.floor(score/NEXTLEVEL)/4;
-        this.dir = dir;
-        this.color = GetRandomColor();
+        this.posX   = pX;
+        this.posY   = pY;
+        
+        if      (dir == DIRECTION.LEFT)  this.speed = GetCurrentBallSpeed();
+        else if (dir == DIRECTION.RIGHT) this.speed = GetCurrentShotSpeed();
+        
+        this.dir   = dir;
+        this.color = color;
     }
-
+    
     //Delete
-    setGradient(ctx) 
+    setGradient() 
     {
         let gradient = ctx.createRadialGradient(this.posX + this.radius / 4,
                                                 this.posY - this.radius / 6, this.radius / 8,
@@ -64,11 +55,11 @@ class TBall
 
     Move() 
     {
-        if (this.dir == Direction.LEFT) this.posX -= this.speed;
+        if (this.dir == DIRECTION.LEFT) this.posX -= this.speed;
         else                            this.posX += this.speed;
     }
 
-    Draw(ctx) 
+    Draw() 
     {
         ctx.fillStyle = this.setGradient(ctx);
         ctx.beginPath();
@@ -78,44 +69,50 @@ class TBall
     }
 }
 
-function CreateItem(x, y) 
-{
-    GetRandomColor();
-
-    return new TBall(x, y, direction); //From where do I get direction?
-}
-
-function DrawBackground(ctx) 
-{
-    ctx.save();
-
-    let g = ctx.createLinearGradient(0, 0, 0, canvas.height); //Fuck the gradient
-
-    g.addColorStop(1, "#202020");
-    g.addColorStop(0, "#aaa")
-    ctx.fillStyle = g;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.strokeRect(0, 0, 50, canvas.height);
-    
-    ctx.restore();
-}
-
+/*Starting new game*/
 function Init() 
 {
     canvas = document.getElementById('canvas');
-    canvas.width = window.innerWidth;
+    canvas.width  = window.innerWidth;
     canvas.height = window.innerHeight / 2;
 
     if (canvas.getContext) 
     {
         ctx = canvas.getContext('2d');
-        DrawBackground(ctx);
         GunColor = GetRandomColor();
-        LBall()
+        best = 0;
+        
+        DrawBackground();
+        SetDefault();
+        DrawBalls()
     }
 }
 
-function LBall()
+function SetDefault()
+{
+    score   = STARTSCORE;
+    level   = STARTLEVEL;
+    randClr = GetRandomColor();
+}
+
+/* Draw */
+function DrawBackground() 
+{
+    ctx.save();
+
+    let g = ctx.createLinearGradient(0, 0, 0, canvas.height); //Get rid of gradient
+
+    g.addColorStop(1, "#202020");
+    g.addColorStop(0, "#aaa");
+    
+    ctx.fillStyle = g;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.strokeRect(0, 0, LEFTBORDER, canvas.height);
+    
+    ctx.restore();
+}
+
+function DrawBalls()
 {
     for (let i = 11; i > 0; i--) 
     {
@@ -123,105 +120,12 @@ function LBall()
         {
             let posX = canvas.width;
             let posY = canvas.height - i*33 + 5;
-            let item = CreateItem(posX, posY);
+            let item = new TBall(posX, posY, "LEFT", GetRandomColor());
             
-            item.Draw(ctx);
+            item.Draw();
             Items.push(item);
         }
     }
-}
-
-function GetMousePos(evt) 
-{
-    return( 
-    {
-        x: evt.clientX - canvas.offsetLeft,
-        y: evt.clientY - canvas.offsetTop
-    });
-}
-
-function MakeShot(event) 
-{
-    if (idTimer != -1 && reloadGun == 1)
-    {
-        reloadGun = 0;
-        
-        let item = CreateItem(50, GetMousePos(event).y);
-        
-        item.color = GunColor;
-        item.speed = 5 + level/2;
-        item.dir = Direction.RIGHT;
-        
-        GunColor = GetRandomColor(); //Maybe use new TBall ?
-        
-        item.Draw(ctx);
-        Items.push(item);
-        DrawGun();
-    }
-}
-
-function ChangeSpeed() 
-{
-    for(let i = 0; i < Items.length; i++)
-    {
-        if (Items[i].dir == Direction.LEFT) Items[i].speed = STARTSPEED + Math.floor(score/NEXTLEVEL)/8;
-        else                                Items[i].speed = 5 + level/2;
-    }
-}
-
-function Collisions(a, b) 
-{
-    let result = Math.sqrt((a.posX - b.posX) ** 2 + (a.posY - b.posY) ** 2) <= a.radius + b.radius;
-
-    if (a.color == b.color || a.dir == Direction.LEFT == b.dir)
-    {
-        return result;
-    }
-    else if (result)
-    {
-        a.dir = Direction.LEFT;
-        a.speed = STARTSPEED + Math.floor(score/NEXTLEVEL)/4;
-        b.dir = Direction.LEFT;
-        b.speed = STARTSPEED + Math.floor(score/NEXTLEVEL)/4;
-    }
-}
-
-function CheckCollisions() 
-{
-    let collided = new Set();
-
-    for (let i = 0; i < Items.length - 1; ++i) 
-    {
-        for (let j = i + 1; j < Items.length; ++j) 
-        {
-            if (Collisions(Items[i], Items[j])) 
-            {
-                collided.add(i);
-                collided.add(j);
-            }
-        }
-    }
-
-    collided = Array.from(collided).sort((a, b) => b - a);
-
-    for (let i of collided) 
-    {
-        Items.splice(i, 1);
-        score = score + 5/2;
-        document.getElementById("Score").value = score;
-    }
-
-    if (level < 1 + Math.floor(score/NEXTLEVEL))
-    {
-        level = 1 + Math.floor(score/NEXTLEVEL);
-        ChangeSpeed();
-        document.getElementById("Level").value = level;
-    }
-}
-
-function SetGunPos(evt)
-{ 
-    Gun = GetMousePos(evt);
 }
 
 function DrawGun() 
@@ -235,30 +139,117 @@ function DrawGun()
     ctx.restore();
 }
 
-function MoveItems() //Bad naming
+/* on mouse move */
+function GetMousePos(evt) 
 {
-    DrawBackground(ctx);
-    CheckCollisions();
+    return( 
+    {
+        x: evt.clientX - canvas.offsetLeft,
+        y: evt.clientY - canvas.offsetTop
+    });
+}
 
-    for (let i = 0; i < Items.length;) 
+function SetGunPos(evt)
+{ 
+    Gun = GetMousePos(evt);
+}
+
+/* on mouse click */
+function MakeShot(event) 
+{
+    if (idTimer != -1 && reloadGun == 1)
+    {
+        reloadGun = 0;
+        
+        let item = new TBall(LEFTBORDER, GetMousePos(event).y, "RIGHT", GunColor);
+                
+        GunColor = GetRandomColor();
+        item.Draw();
+        Items.push(item);
+        DrawGun();
+    }
+}
+
+/* Collisions */
+function CheckCollisions() 
+{
+    let collided = new Set();
+
+    for (let i = 0; i < Items.length - 1; ++i)
+    {
+        for (let j = i + 1; j < Items.length; ++j) 
+        {
+            if (IsCollided(Items[i], Items[j])) 
+            {
+                collided.add(i);
+                collided.add(j);
+            }
+        }
+    }
+
+    collided = Array.from(collided).sort((a, b) => b - a);
+
+    for (let i of collided) 
+    {
+        Items.splice(i, 1);
+        score += 5/2;
+        document.getElementById("Score").value = score;
+    }
+
+    if (level < GetCurrentLevel())
+    {
+        level = GetCurrentLevel();
+        document.getElementById("Level").value = level;
+        
+        ChangeSpeed();
+    }
+}
+
+function IsCollided(a, b) 
+{
+    let result = Math.sqrt((a.posX - b.posX) ** 2 + (a.posY - b.posY) ** 2) <= a.radius + b.radius; // Add > 2 collided objecs
+
+    if (a.color == b.color)
+    {
+        return result;
+    }
+    
+    if (result)
+    {
+        a.dir   = b.dir   = "LEFT";
+        a.speed = b.speed = GetCurrentBallSpeed();
+    }
+    
+    return 0;
+}
+
+/* Timer functions */
+function MoveItems() 
+{
+    DrawBackground();
+    CheckCollisions();
+    
+    let i = 0;
+
+    while(i < Items.length) 
     {
         Items[i].Move();
-        Items[i].Draw(ctx);
+        Items[i].Draw();
 
-        if (Items[i].posX <= 50 && Items[i].dir == Direction.LEFT)
+        if (Items[i].posX <= LEFTBORDER && Items[i].dir == DIRECTION.LEFT)
         {
-            clearInterval(idTimer);
-            idTimer=-1;
-            clearInterval(Timer);
-            Items.splice(0, Items.length);
-            level = 1; //add function SetDefault()
-            score = 0;
+            if (best < score) best = score;
+            
+            ResetTimers();
+            SetDefault();
+            Items = [];
+            
+            document.getElementById("best").value = best;
             document.getElementById("Level").value = level;
             document.getElementById("Score").value = score;
-            alert("Вы проиграли!");
+            alert("Вы проиграли!"); // Boring
         }
-        else if (Items[i].posX - canvas.width > 0 || Items[i].posX < 0 ||
-                 Items[i].posY < 0 || Items[i].posY - canvas.height > 0)
+        else if (Items[i].posX > canvas.width)
         {
             Items.splice(i, 1);
         }
@@ -276,12 +267,13 @@ function GetBullet()
     reloadGun = 1;
 }
 
+/* Timers handling */
 function SetTimers() 
 {
     if (idTimer === -1)
     {
-        idTimer = setInterval(MoveItems, 30);
-        Timer = setInterval(LBall, 2000 + level*3000);
+        idTimer  = setInterval(MoveItems, 30);
+        Timer    = setInterval(DrawBalls, 2000 + level*3000);
         GunTimer = setInterval(GetBullet, 500);
     }
 }
@@ -292,5 +284,54 @@ function ResetTimers()
     clearInterval(Timer);
     clearInterval(GunTimer);
     
-    idTimer=-1;
+    if (idTimer == -1) SetTimers();
+    else               idTimer = -1;
+}
+
+/* Calc current attributes */
+function GetCurrentLevel()
+{
+    return (1 + Math.floor(score/NEXTLEVEL));
+}
+
+function GetCurrentBallSpeed()
+{
+    return (STARTSPEED + Math.floor(score/NEXTLEVEL) / 4);
+}
+
+function GetCurrentShotSpeed()
+{
+    return (5 + level / 2);
+}
+
+/* Utility functions */
+function GetRandomColor() 
+{
+    let keys = Object.keys(COLORS);
+
+    return COLORS[keys[Math.floor(keys.length * Math.random())]];
+}
+
+function ChangeSpeed() 
+{   
+    for(let i = 0; i < Items.length; i++)
+    {
+        if (Items[i].dir == DIRECTION.LEFT) Items[i].speed = GetCurrentBallSpeed();
+        else                                Items[i].speed = GetCurrentShotSpeed();
+    }
+}
+
+/* Keybord handlers */
+function HandleKeys(event)
+{
+    let key = event.keyCode;
+    
+    switch(key)
+    {
+        case 32:
+            ResetTimers();
+            break;
+        default:
+            break;
+    }
 }
